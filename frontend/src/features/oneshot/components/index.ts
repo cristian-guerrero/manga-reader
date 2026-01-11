b98ec00ea63cc238bb500e8ab8d6a73d185d3df1
Cristian Guerrero <guerrero.chavez.cristian@gmail.com>
2026-01-11 15:17:46 -0500

    fixed oneshot ts errors

--- frontend/src/features/oneshot/components/index.ts
+++ frontend/src/features/oneshot/components/index.ts
@@ -2,7 +2,7 @@
  * OneShot Components - Barrel export
  */
 
-export { OneShotIcons } from './OneShotIcons';
+export { OneShotIcon, PlusIcon, TrashIcon, ImageIcon } from './OneShotIcons';
 export { OneShotHeader } from './OneShotHeader';
 export { OneShotGrid } from './OneShotGrid';
 export { OneShotLoadingState } from './OneShotLoadingState';
