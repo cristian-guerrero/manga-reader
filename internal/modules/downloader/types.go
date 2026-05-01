9e47bfa2ac8d7d826519c798698aa3db286681ca
cristian guerrero <guerrero.chavez.cristian@gmail.com>
2026-04-30 23:14:45 -0500

    fixed mangadex

--- internal/modules/downloader/types.go
+++ internal/modules/downloader/types.go
@@ -3,10 +3,13 @@ package downloader
 import "time"
 
 type ImageDownload struct {
-	URL      string
-	Filename string
-	Index    int
-	Headers  map[string]string
+	URL         string
+	Filename    string
+	Index       int
+	Headers     map[string]string
+	SkipHeaders bool // When true, no headers will be sent (required for some CDNs like MangaDex)
+	// Original filename from the source (needed for URL refresh on some sites)
+	SourceFilename string
 }
 
 type ChapterInfo struct {
@@ -27,6 +30,8 @@ type SiteInfo struct {
 	// New fields for series support
 	Type     string // "single" or "series"
 	Chapters []ChapterInfo
+	// Extra data for downloaders that need to refresh URLs (e.g., MangaDex chapter ID)
+	Extra map[string]string
 }
 
 type DownloaderInterface interface {
